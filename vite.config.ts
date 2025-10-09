81c8eb40c92555eff0374d909b693f32b35f64cc
surya-msp <suryamsp2022@gmail.com>
2025-10-09 17:49:30 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,26 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-    esbuildOptions: {
-      target: 'esnext'
+  resolve: {
+    alias: {
+      '@mui/material': '@mui/material',
+      '@mui/icons-material': '@mui/icons-material'
     }
+  },
+  optimizeDeps: {
+    include: [
+      '@mui/material',
+      '@mui/icons-material',
+      '@emotion/react',
+      '@emotion/styled',
+      'react',
+      'react-dom'
+    ],
+    exclude: ['lucide-react']
   },
   server: {
     fs: {
       strict: false
-    }
-  },
-  build: {
-    target: 'esnext',
-    commonjsOptions: {
-      include: [/node_modules/]
     }
   }
 });
